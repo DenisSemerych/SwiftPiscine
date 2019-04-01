400cae0ae4984c71815d11e540ad67787eb729e5
DenisSemerych <denis_semerych@icloud.com>
2019-04-01 14:47:05 +0300

    00

---
+++ d00/ex00/ex00/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by Denis SEMERYCH on 4/1/19.
+//  Copyright © 2019 Denis SEMERYCH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func printMsgToConsole(_ sender: UIButton) {
+        print("Hello World!")
+    }
+    
+}
+
